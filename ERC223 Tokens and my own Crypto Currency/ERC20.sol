35890695053c0f05cfcddfad644945d1e9f5af54
Hayden Fan <haosong0323@gmail.com>
2021-08-03 00:00:41 +0800

    feat: add ERC223 Tokens and my own Crypto Currency

---
+++ ERC223 Tokens and my own Crypto Currency/ERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+interface ERC20 {
+  function transferFrom(address _from, address _to, uint _value) external returns (bool);
+  function approve(address _spender, uint _value) external returns (bool);
+  function allowance(address _owner, address _spender) external returns (uint);
+  event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
